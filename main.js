36e8523fbfa792111a2e8741a0deee7e14272b43
Jose <enriquesais123@gmail.com>
2023-07-27 19:37:11 -0500

    menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const menuHamIcon=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click',tooleDesktopMenu);
+menuHamIcon.addEventListener('click',tooleMobileMenu);
 
 function tooleDesktopMenu(){
 desktopMenu.classList.toggle('inactive');
+}
+function tooleMobileMenu(){
+mobileMenu.classList.toggle('inactive');
 }
